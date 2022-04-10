9ed915e07a9bc3977bace25ae4b0d9717b64445e
harshshahashah <73783032+harshshahashah@users.noreply.github.com>
2022-04-10 11:14:24 +0530

    Add files via upload

---
+++ List_from_dictionary.py
@@ -0,0 +1,8 @@
+Dict = {
+    "Language" : ["C" , "C++" , "Java" , "Python"],
+    "Core" : ["CAD" , "CAM" , "CAE"],
+    "Software" : ["Solidworks" , "Ansys" , "AutoCAD" , "Catia"]
+    }
+
+print("Accessing Core : ",Dict["Core"])
+print("Accessing something from Core : ",Dict["Core"][1])
