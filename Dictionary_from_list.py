e79e118e168bfd17d96d275b9c13dc2cb54a706a
harshshahashah <73783032+harshshahashah@users.noreply.github.com>
2022-04-10 11:08:31 +0530

    Add files via upload

---
+++ Dictionary_from_list.py
@@ -0,0 +1,3 @@
+List = [{"CAD" : "Solidworks" , "CAM" : "Mastercam"} , {"CAE" : "Ansys" , "CFX" : "Fluent"} , {"Core" : "Mech" , "Circuital" : "Elect"}]
+print("Accessed CAD : ",List[0]["CAD"])
+print("Accessed Core : ",List[2]["Core"])
